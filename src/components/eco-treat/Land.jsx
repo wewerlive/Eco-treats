09d935fea840ab83e20d2ff4cdda2060f0d3f598
Bhaumic-kun <bhaumiksingh2000@gmail.com>
2023-01-12 08:53:16 +0530

    Eco: land as individual component

--- src/components/eco-treat/Land.jsx
+++ src/components/eco-treat/Land.jsx
@@ -1,26 +1,21 @@
-import { Sky, useTexture } from '@react-three/drei';
+import { useTexture } from '@react-three/drei';
 import { RigidBody } from '@react-three/rapier';
-import Clouds from '../canvas/Clouds';
+import EnvMap from '../canvas/EnvMap';
 
 export default function Land({ children, ...props }) {
   const land = useTexture('/img/textures/land.jpg');
   return (
-    <>
-      <Sky
-        distance={450000}
-        sunPosition={[250, 100, 250]}
-        inclination={0}
-        azimuth={0.25}
-      />
+    <EnvMap>
       <RigidBody
         colliders={'hull'}
         type='fixed'
       >
         <mesh
           {...props}
-          position={[0, -20, 0]}
-          rotation={[-0.5 * Math.PI, 0, 0]}>
-          <planeGeometry args={[500, 500, 1, 1, 0]} />
+          position={[0, 0, 0]}
+          rotation={[-0.5 * Math.PI, 0, 0]}
+        >
+          <planeGeometry args={[1000, 1000, 1, 1, 0]} />
           <meshPhongMaterial
             map={land}
             toneMapped={false}
@@ -29,14 +24,6 @@ export default function Land({ children, ...props }) {
         </mesh>
       </RigidBody>
       {children}
-      <Clouds
-        opacity={1.26}
-        speed={2}
-        depth={6.7}
-        segments={200}
-        color={'#ffffff'}
-        width={200}
-      />
-    </>
+    </EnvMap>
   );
 }
