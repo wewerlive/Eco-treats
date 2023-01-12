e72c268ad2a86711bd6407d04ffd0e7204b85577
Bhaumic-kun <bhaumiksingh2000@gmail.com>
2023-01-12 08:51:05 +0530

    Eco: added lights & model

--- src/components/eco-treat/Park.jsx
+++ src/components/eco-treat/Park.jsx
@@ -1,21 +1,39 @@
 import Land from './Land';
 import { extend } from '@react-three/fiber';
 import dynamic from 'next/dynamic';
-import Player from './Player';
+import Lights from '../canvas/Lights';
+import Ocean from '../canvas/Ocean';
 
 const Model = dynamic(() => import('./Model'), { ssr: true });
 
-extend({ Land, Model});
+extend({ Land, Model });
 
 export default function Park({ children, ...props }) {
   return (
     <Land receiveShadow>
-      <Model objectName={'glbs/tentView.glb'}/>
-      <Player
-        colliders={'hull'}
-        mass={1}
-        type='dynamic'
+      <Lights
+        position={[47, 10, 57]}
+        color={'#fefefe'}
       />
+      <Lights
+        position={[-1.2, 10, 22]}
+        color={'#fefefe'}
+      />
+      <Model
+        colliders='trimesh'
+        position={[50, 0, 20]}
+        type='fixed'
+        objectName={'glbs/tent.glb'}
+      />
+      <Lights
+        position={[50, 10, -27]}
+        color={'#fefefe'}
+      />
+      <Lights
+        position={[96, 10, 26]}
+        color={'#fefefe'}
+      />
+      <Ocean position={[50, 0.1, 20]} />
     </Land>
   );
 }
