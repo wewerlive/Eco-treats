6d8357607d533549be268980d2506ad6b70e686f
Bhaumic-kun <bhaumiksingh2000@gmail.com>
2023-01-12 08:54:13 +0530

    Eco: land and player into scene

--- src/components/canvas/Scene.jsx
+++ src/components/canvas/Scene.jsx
@@ -2,6 +2,10 @@ import { Canvas } from '@react-three/fiber';
 import { KeyboardControls, OrbitControls, Preload } from '@react-three/drei';
 import { Debug, Physics } from '@react-three/rapier';
 import { Controllers, Hands, XR } from '@react-three/xr';
+import dynamic from 'next/dynamic';
+
+const Land = dynamic(() => import('../eco-treat/Land'), { ssr: true });
+const Player = dynamic(() => import('../eco-treat/Player'), { ssr: true });
 
 export default function Scene({ children, ...props }) {
   // Everything defined in here will persist between route changes, only children are swapped
@@ -15,25 +19,24 @@ export default function Scene({ children, ...props }) {
         { name: 'jump', keys: ['Space'] },
         { name: 'rLeft', keys: ['ArrowLeft'] },
         { name: 'rRight', keys: ['ArrowRight'] },
-      ]}>
+      ]}
+    >
       <Canvas
         shadows
         camera={{ fov: 45 }}
-        {...props}>
+        {...props}
+      >
         <XR>
           <Hands />
           <Controllers />
-          <directionalLight intensity={0.75} />
-          <ambientLight intensity={0.75} />
-          <Physics gravity={[0, -600, 0]}>
-            {children}
-            {/* <Debug /> // Debug is optional */}
+          <directionalLight intensity={1} />
+          <Physics gravity={[0, -200, 0]}>
+            <Land>
+              {children}
+              <Player />
+            </Land>
           </Physics>
           <Preload all />
-          <OrbitControls
-            enableDamping={true}
-            maxPolarAngle={Math.PI}
-          />
         </XR>
       </Canvas>
     </KeyboardControls>
