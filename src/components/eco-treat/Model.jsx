14b30b4a5d7c284c6630dea401383b2cdb88a4b6
Bhaumic-kun <bhaumiksingh2000@gmail.com>
2023-01-12 08:51:46 +0530

    Eco: enabled model replication

--- src/components/eco-treat/Model.jsx
+++ src/components/eco-treat/Model.jsx
@@ -1,20 +1,28 @@
 import { useLoader } from '@react-three/fiber';
 import { RigidBody } from '@react-three/rapier';
+import { useMemo } from 'react';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import * as SkeletonUtils from 'three/examples/jsm/utils/SkeletonUtils.js';
+import { extend } from '@react-three/fiber';
 
-export default function Model({ objectName }) {
-  const { scene, nodes } = useLoader(GLTFLoader, objectName);
+extend({ SkeletonUtils, GLTFLoader })
+
+export default function Model({ objectName, type, position, colliders }) {
+  const { scene } = useLoader(GLTFLoader, objectName);
+  const object = useMemo(() => SkeletonUtils.clone(scene), [scene]);
   return (
-    <group>
+    <group position={position}>
       {Object.keys(scene.children).map((f, index) => (
         <RigidBody
-          colliders='hull'
+          colliders={colliders}
           key={index}
-          type='fixed'>
+          type={type}
+        >
           <primitive
             key={index}
-            object={scene.children[f]}
+            object={object.children[f]}
             position={scene.children[f].position}
+            scale={[0.1, 0.1, 0.1]}
           />
         </RigidBody>
       ))}
