ee51c4d730b486a2afc0ec9897e8414c415d85f3
Bhaumic-kun <bhaumiksingh2000@gmail.com>
2023-01-13 14:35:55 +0530

    Eco: fixed model & light issues

--- src/components/eco-treat/Model.jsx
+++ src/components/eco-treat/Model.jsx
@@ -2,14 +2,14 @@ import { useLoader } from '@react-three/fiber';
 import { RigidBody } from '@react-three/rapier';
 import { useMemo } from 'react';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import * as SkeletonUtils from 'three/examples/jsm/utils/SkeletonUtils.js';
+import { clone } from 'three/examples/jsm/utils/SkeletonUtils.js';
 import { extend } from '@react-three/fiber';
 
-extend({ SkeletonUtils, GLTFLoader })
+extend({ SkeletonUtils, GLTFLoader });
 
 export default function Model({ objectName, type, position, colliders }) {
   const { scene } = useLoader(GLTFLoader, objectName);
-  const object = useMemo(() => SkeletonUtils.clone(scene), [scene]);
+  const object = useMemo(() => clone(scene), [scene]);
   return (
     <group position={position}>
       {Object.keys(scene.children).map((f, index) => (
--- src/components/eco-treat/Park.jsx
+++ src/components/eco-treat/Park.jsx
@@ -1,7 +1,6 @@
 import Land from './Land';
 import { extend } from '@react-three/fiber';
 import dynamic from 'next/dynamic';
-import Lights from '../canvas/Lights';
 import Ocean from '../canvas/Ocean';
 
 const Model = dynamic(() => import('./Model'), { ssr: true });
@@ -11,27 +10,12 @@ extend({ Land, Model });
 export default function Park({ children, ...props }) {
   return (
     <Land receiveShadow>
-      <Lights
-        position={[47, 10, 57]}
-        color={'#fefefe'}
-      />
-      <Lights
-        position={[-1.2, 10, 22]}
-        color={'#fefefe'}
-      />
+      <ambientLight />
       <Model
         colliders='trimesh'
         position={[50, 0, 20]}
         type='fixed'
         objectName={'glbs/tent.glb'}
-      />
-      <Lights
-        position={[50, 10, -27]}
-        color={'#fefefe'}
-      />
-      <Lights
-        position={[96, 10, 26]}
-        color={'#fefefe'}
       />
       <Ocean position={[50, 0.1, 20]} />
     </Land>
