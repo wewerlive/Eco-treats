6ef78933adad5a94b4b7ac41cb9d4e0e2f624eca
Bhaumic-kun <bhaumiksingh2000@gmail.com>
2023-01-13 14:51:20 +0530

    Eco: changed skeletonUtils import

--- src/components/eco-treat/Model.jsx
+++ src/components/eco-treat/Model.jsx
@@ -2,30 +2,30 @@ import { useLoader } from '@react-three/fiber';
 import { RigidBody } from '@react-three/rapier';
 import { useMemo } from 'react';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { clone } from 'three/examples/jsm/utils/SkeletonUtils.js';
+import * as SkeletonUtils from 'three/examples/jsm/utils/SkeletonUtils.js';
 import { extend } from '@react-three/fiber';
 
 extend({ SkeletonUtils, GLTFLoader });
 
 export default function Model({ objectName, type, position, colliders }) {
   const { scene } = useLoader(GLTFLoader, objectName);
-  const object = useMemo(() => clone(scene), [scene]);
+  const object = useMemo(() => SkeletonUtils.clone(scene), [scene]);
   return (
-    <group position={position}>
-      {Object.keys(scene.children).map((f, index) => (
-        <RigidBody
-          colliders={colliders}
-          key={index}
-          type={type}
-        >
-          <primitive
+      <group position={position}>
+        {Object.keys(scene.children).map((f, index) => (
+          <RigidBody
+            colliders={colliders}
             key={index}
-            object={object.children[f]}
-            position={scene.children[f].position}
-            scale={[0.1, 0.1, 0.1]}
-          />
-        </RigidBody>
-      ))}
-    </group>
+            type={type}
+          >
+            <primitive
+              key={index}
+              object={object.children[f]}
+              position={scene.children[f].position}
+              scale={[0.1, 0.1, 0.1]}
+            />
+          </RigidBody>
+        ))}
+      </group>
   );
 }
