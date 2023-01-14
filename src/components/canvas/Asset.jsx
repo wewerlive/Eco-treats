86f871efc283a575e977205e65d60621d2133538
Bhaumic-kun <bhaumiksingh2000@gmail.com>
2023-01-14 13:32:19 +0530

    Eco: some WIP* components

---
+++ src/components/canvas/Asset.jsx
@@ -0,0 +1,21 @@
+import { useGLTF } from '@react-three/drei';
+import { RigidBody } from '@react-three/rapier';
+import { useMemo } from 'react';
+import { SkeletonUtils } from 'three-stdlib';
+
+export default function Assest({ colliders, type, object, scale, position }) {
+  const { scene } = useGLTF(object, true, true);
+  const assets = useMemo(() => SkeletonUtils.clone(scene), [scene]);
+  return (
+    <RigidBody
+      colliders={colliders}
+      type={type}
+    >
+      <primitive
+        object={assets}
+        position={position}
+        scale={scale}
+      />
+    </RigidBody>
+  );
+}
--- src/components/eco-treat/Park.jsx
+++ src/components/eco-treat/Park.jsx
@@ -2,22 +2,28 @@ import Land from './Land';
 import { extend } from '@react-three/fiber';
 import dynamic from 'next/dynamic';
 import Ocean from '../canvas/Ocean';
+import Asset from '../canvas/Asset';
+import { RigidBody } from '@react-three/rapier';
+import { useGLTF } from '@react-three/drei';
 
-const Model = dynamic(() => import('./Model'), { ssr: true });
+const Player = dynamic(() => import('./Player'), { ssr: true });
+const Model = dynamic(() => import('../canvas/Model'), { ssr: true });
 
-extend({ Land, Model });
+extend({ Land, Model, Player, Ocean });
 
 export default function Park({ children, ...props }) {
+  const { scene } = useGLTF('glbs/beachSand.glb', true, true);
   return (
     <Land receiveShadow>
       <ambientLight />
-      <Model
+      {/* <Model
         colliders='trimesh'
         position={[50, 0, 20]}
         type='fixed'
         objectName={'glbs/tent.glb'}
-      />
-      <Ocean position={[50, 0.1, 20]} />
+      /> */}
+      <Player />
+      {/* <Ocean position={[50, 0.1, 20]} /> */}
     </Land>
   );
 }
