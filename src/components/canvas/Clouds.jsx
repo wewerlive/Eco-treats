1e2255b3c9541946ea7f15da1f47ff799a0071d4
Bhaumic-kun <bhaumiksingh2000@gmail.com>
2023-01-14 13:28:20 +0530

    Eco: risky Cloud component

--- src/components/canvas/Clouds.jsx
+++ src/components/canvas/Clouds.jsx
@@ -1,20 +1,25 @@
 import { Cloud } from "@react-three/drei";
 
 // texture={'https://i.imgur.com/9Ii9kvp.png'} // adds image to a cloud
-export default function Clouds({...props}) {
+{
+  /* // cloud needs some time to be perfect */
+}
+
+// ! DONOT USE THIS COMPONENT
+// ! DONOT USE THIS COMPONENT
+// ! DONOT USE THIS COMPONENT
+export default function Clouds({ ...props }) {
   return (
-    <>
-      {/* // cloud needs some time to be perfect */}
-      <Cloud
-        {...props}
-        position={[-100, 200, 10]} // Position
-        depthTest={false} // Disable depth test
-      />
-      <Cloud
-        {...props}
-        position={[200, 200, 10]} // Position
-        depthTest={false} // Disable depth test
-      />
-    </>
+    <Cloud
+      {...props}
+      opacity={1.26}
+      speed={10}
+      depth={10}
+      segments={1000}
+      color={'#ffffff'}
+      width={700}
+      position={[100, 800, 10]} // Position
+      depthTest={false} // Disable depth test
+    />
   );
 }
