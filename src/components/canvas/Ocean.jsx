38dd318e0c0a0dc62d070c0d00dd68094df76081
Bhaumic-kun <bhaumiksingh2000@gmail.com>
2023-02-15 13:48:58 +0530

    Eco: replaced with calm ocean texture

--- src/components/canvas/Ocean.jsx
+++ src/components/canvas/Ocean.jsx
@@ -1,6 +1,7 @@
+"{use strict}";
 import React, { useRef, useMemo } from 'react';
 import { extend, useThree, useLoader, useFrame } from '@react-three/fiber';
-import * as THREE from 'three';
+import { TextureLoader, Vector3, PlaneGeometry, RepeatWrapping } from 'three';
 
 import { Water } from 'three/examples/jsm/objects/Water.js';
 
@@ -10,18 +11,18 @@ export default function Ocean({ ...props }) {
   const ref = useRef();
   const gl = useThree((state) => state.gl);
   const waterNormals = useLoader(
-    THREE.TextureLoader,
-    'https://static.wixstatic.com/media/38ebb6_50c433eba3864b91b73979c9028d21c2~mv2.jpg?dn=water%200342normal.jpg',
+    TextureLoader,
+    'img/textures/waterTexture.jpg',
   );
 
-  waterNormals.wrapS = waterNormals.wrapT = THREE.RepeatWrapping;
-  const geom = useMemo(() => new THREE.PlaneGeometry(500, 1000), []);
+  waterNormals.wrapS = waterNormals.wrapT = RepeatWrapping;
+  const geom = useMemo(() => new PlaneGeometry(500, 1000), []);
   const config = useMemo(
     () => ({
       textureWidth: 512,
       textureHeight: 512,
       waterNormals,
-      sunDirection: new THREE.Vector3(),
+      sunDirection: new Vector3(),
       sunColor: 0xeb8934,
       waterColor: 0x0064b5,
       distortionScale: 40,
