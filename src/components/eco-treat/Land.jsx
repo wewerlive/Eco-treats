bfd77dfe2a0d98c1062251d531daf2748a1c78a3
Bhaumic-kun <bhaumiksingh2000@gmail.com>
2023-02-15 13:46:29 +0530

    Eco: fixed land with a sample texture

--- src/components/eco-treat/Land.jsx
+++ src/components/eco-treat/Land.jsx
@@ -3,48 +3,26 @@ import { RigidBody } from '@react-three/rapier';
 import Beach from '../canvas/Beach';
 
 export default function Land({ children, ...props }) {
-  // const land = useTexture('/img/textures/land.jpg');
+  const land = useTexture('/img/textures/land.jpg');
 
   return (
     <>
       <RigidBody type='fixed'>
-        <Beach position={[10, 1, 10]} />
-        <Beach position={[20, 1, 20]} />
+        <mesh
+          {...props}
+          castShadow
+          position={[0, 0, 0]}
+          rotation={[-0.5 * Math.PI, 0, 0]}
+        >
+          <planeGeometry args={[1000, 1000, 1, 1, 0]} />
+          <meshPhongMaterial
+            map={land}
+            toneMapped={false}
+            shininess={0.5}
+          />
+        </mesh>
       </RigidBody>
       {children}
     </>
   );
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{
-  /* <mesh
-  {...props}
-  castShadow
-  position={[0, 0, 0]}
-  rotation={[-0.5 * Math.PI, 0, 0]}
->
-  <planeGeometry args={[1000, 1000, 1, 1, 0]} />
-  <meshPhongMaterial
-    map={land}
-    toneMapped={false}
-    shininess={0.5}
-  />
-</mesh>; */
 }
--- src/components/eco-treat/Park.jsx
+++ src/components/eco-treat/Park.jsx
@@ -2,9 +2,7 @@ import Land from './Land';
 import { extend } from '@react-three/fiber';
 import dynamic from 'next/dynamic';
 import Ocean from '../canvas/Ocean';
-import Asset from '../canvas/Asset';
-import { RigidBody } from '@react-three/rapier';
-import { useGLTF } from '@react-three/drei';
+// import Asset from '../canvas/Asset';
 
 const Player = dynamic(() => import('./Player'), { ssr: true });
 const Model = dynamic(() => import('../canvas/Model'), { ssr: true });
@@ -12,18 +10,17 @@ const Model = dynamic(() => import('../canvas/Model'), { ssr: true });
 extend({ Land, Model, Player, Ocean });
 
 export default function Park({ children, ...props }) {
-  const { scene } = useGLTF('glbs/beachSand.glb', true, true);
   return (
     <Land receiveShadow>
       <ambientLight />
+      <Ocean position={[500, 0.1, 10]} />
       <Model
         colliders='trimesh'
-        position={[50, 0, 20]}
+        position={[50, 0.1, 20]}
         type='fixed'
         objectName={'glbs/tent.glb'}
       />
       <Player />
-      <Ocean position={[50, 0.1, 20]} />
     </Land>
   );
 }
